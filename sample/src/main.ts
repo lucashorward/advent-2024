5795397c07521858401a4f7b09ec87ec367cd426
Lucas Horward <lucas.horward@gmail.com>
2024-12-07 09:33:59 +0100

    Day 7 part 1

--- sample/src/main.ts
+++ sample/src/main.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 
 const inputFolder = 'sample/__input__';
-const file = fs.readFileSync(`${inputFolder}/real.txt`, 'utf-8');
+const file = fs.readFileSync(`${inputFolder}/test.txt`, 'utf-8');
 
 const lines = file.split('\n');
 
